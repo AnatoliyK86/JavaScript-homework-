8afd62c8c0104c1ede87842f3b6ca1ab1481057d
Anatolii86 <kozachenkoav@gmail.com>
2021-12-07 22:27:52 +0200

    все исправил

--- Homework_3/part-4/task-4_1.js
+++ Homework_3/part-4/task-4_1.js
@@ -3,14 +3,29 @@ let sum = 0;
 let count = 0;
 let i = 0;
 
-while (count <= 10) {
-	if ((i % 2 == 0) && (i % 3 == 0)){
+while (count < 10) {
+	i++;
+	if ((i % 2 !== 0) || (i % 3 !== 0)) continue;
 		sum = sum + i;
 		count++;
-	}
-	i++;
+	console.log(sum);
+	
 }
 alert (`Cумма первых 10 чётных чисел, которые делятся на 3 = ${sum}`);
+
+
+// let sum = 0;
+// let count = 0;
+// let i = 0;
+
+// while (count <= 10) {
+// 	if ((i % 2 == 0) && (i % 3 == 0)){
+// 		sum = sum + i;
+// 		count++;
+// 	}
+// 	i++;
+// }
+// alert (`Cумма первых 10 чётных чисел, которые делятся на 3 = ${sum}`);
 
 
 
--- Homework_3/part-4/task-4_2.js
+++ Homework_3/part-4/task-4_2.js
@@ -1,12 +1,39 @@
-let num = 30;
-let maxNum;
 
-nextPrime:
-for (let i = 2; i <= num; i++) { 
 
-	for (let j = 2; j < i; j++) { 
-		if (i % j == 0) continue nextPrime; 
+	let num = 9;
+	let maxNum;
+	
+	for (let i = 2; i <= num; i++) { 
+		if (checkPrime(i)) {
+			maxNum = i;
+		}
 	}
-	maxNum = i;
-}
-	alert(`Наибольшее простое число, которое не больше числа ${num} это: ${maxNum}`); 
+		alert(`Наибольшее простое число, которое не больше числа ${num} это: ${maxNum}`); 
+	
+	
+		function checkPrime(i) {
+			for (let j = 2; j < i; j++) { 
+				if (i % j == 0) {
+					return false; 
+				}
+			}
+			return true;
+		}
+	
+
+
+
+// let num = 30;
+// let maxNum;
+
+// nextPrime:
+// for (let i = 2; i <= num; i++) { 
+// 	for (let j = 2; j < i; j++) { 
+// 		if (i % j == 0) continue nextPrime; 
+// 	}
+// 	maxNum = i;
+// }
+// 	alert(`Наибольшее простое число, которое не больше числа ${num} это: ${maxNum}`); 
+
+
+
