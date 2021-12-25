bfd79a7c33b05c0a1979172beb56734c5b94b900
Anatolii86 <kozachenkoav@gmail.com>
2021-12-25 22:11:18 +0200

    changed a few things, but not everything worked out

--- Homework_11/script.js
+++ Homework_11/script.js
@@ -1,46 +1,108 @@
+
+// let elem;
+// const getSpecFunc = (elem) => {
+// 	for (let elem of document.querySelectorAll('#link')) {
+// 		elem.href = `http://${prompt(setting.message)}`;
+// 		elem.innerHTML = prompt(setting.message);
+// 		return elem;
+// 	}
+// }
+
+// const settings = [
+// 	{
+// 		message:  'виберіть фон для сторінки:',
+// 		action: val => document.body.style.backgroundColor = val
+// 	},
+// 	{
+// 		message:  'виберіть тип шрифта на сторінці:',
+// 		action: val => document.body.style.fontFamily = val
+// 	},
+// 	{
+// 		message:  'виберіть вирівнювання для заголовка h1:',
+// 		action: val => document.querySelector('#title').style.textAlign = val
+// 	},
+// 	{
+// 		message:  'виберіть фон для параграфа зі зсилками:',
+// 		action: val => document.querySelector('#text_sites').style.backgroundColor = val
+// 	},
+// 	{
+// 		message:  'виберіть колір тексту у параграфі зі зсилками:',
+// 		action: val => document.querySelector('#text_sites').style.color = val
+// 	},
+// 	{
+// 		message:  'виберіть колір тексту у параграфі зі зсилками, для самих зсилок:',
+// 		action: val => document.querySelectorAll('.link').style.color = val
+// 	},
+// 	{
+// 		message:  'виберіть колір тексту у елементі div:',
+// 		action: val => document.querySelector('#text_block').style.color = val
+// 	},
+// 	{
+// 		message:  'виберіть розмір тексту у елементі div:',
+// 		action: val => document.querySelector('#text_block').style.fontSize = val
+// 	},
+// 	{
+// 		message:  'виберіть товщину тексту у елементі div:',
+// 		action: val => document.querySelector('#text_block').style.fontWeight = val
+// 	},
+// 	{
+// 		message:  'виберіть тип маркера для маркованого списку на сторінці:',
+// 		action: val => document.querySelectorAll('#list_item').style.listStyleType = val
+// 	},
+// 	{
+// 		message:  'введіть адресу сайту:',
+// 		action: val => {document.querySelector('#link-1').style.listStyleType = val; 
+// 						elem.href = `http://${prompt(setting.message)}`;
+// 						elem.innerHTML = prompt(setting.message);
+// 						}
+// 	}
+// ]
+
+// for (let setting of settings) {
+// 	const value = prompt(setting.message);
+// 	setting.action(value);
+// }
+
+//-------------
 let bgcB = prompt('виберіть фон для сторінки:', 'yellow');
-let backgroundColorBoby = document.body.style.backgroundColor = bgcB;
+document.body.style.backgroundColor = bgcB;
 
 let fontFam = prompt('виберіть тип шрифта на сторінці:', 'Arial');
-let font_Family = document.body.style.fontFamily = fontFam;
+document.body.style.fontFamily = fontFam;
 
 let align = prompt('виберіть вирівнювання для заголовка h1:', 'center');
-let text_Align = document.querySelector('h1');
-	text_Align.style.textAlign = align;
+document.querySelector('#title').style.textAlign = align;
+	
 
 let bgcP = prompt('виберіть фон для параграфа зі зсилками:', 'pink');
-let backgroundColorP = document.querySelector('p');
-	backgroundColorP.style.backgroundColor = bgcP;
-
+document.querySelector('#text_sites').style.backgroundColor = bgcP;
+	
 let colorP = prompt('виберіть колір тексту у параграфі зі зсилками:', 'red');
-let Color_P = document.querySelector('p');
-	Color_P.style.color = colorP;
+document.querySelector('#text_sites').style.color = colorP;
 
 let colorA = prompt('виберіть колір тексту у параграфі зі зсилками, для самих зсилок:', 'black');
-let A_all = document.querySelectorAll('p > a');
+let A_all = document.querySelectorAll('.link');
 	for (let elem of A_all) {
 		elem.style.color = colorA;
+		let adress = prompt('введіть адресу сайту:','');
+		elem.href = 'http://${adress}'; 
+		elem.innerHTML = adress;
 	}
 
 let colorDiv = prompt('виберіть колір тексту у елементі div:', 'green');
-let divT = document.querySelector('div');
-	divT.style.color = colorDiv;
+document.querySelector('#text_block').style.color = colorDiv;
+	
 
 let fsizeDiv = prompt('виберіть розмір тексту у елементі div:', '18px');
-	divT.style.fontSize = fsizeDiv;
+document.querySelector('#text_block').style.fontSize = fsizeDiv;
 
 let fweightDiv = prompt('виберіть товщину тексту у елементі div:', '600');
-	divT.style.fontWeight = fweightDiv;
+document.querySelector('#text_block').style.fontWeight = fweightDiv;
 
 let liStyle = prompt('виберіть тип маркера для маркованого списку на сторінці:', 'square');
-let li_Items = document.querySelectorAll('ul > li');
+let li_Items = document.querySelectorAll('#list_item');
 	for (let elem of li_Items) {
 		elem.style.listStyleType = liStyle;
 	}
 
-	for (let elem of A_all) {
-		let adress = prompt('введіть адресу сайту:','');
-		elem.setAttribute('href', `http://${adress}`);
-		elem.innerHTML = adress;
-	}
 
